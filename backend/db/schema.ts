b4881ce07e8c8a9771d64f55cfe2b085f2a5ef94
Tikhon <tikhon@wevestr.com>
2024-04-15 16:39:46 +0300

    user migration

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -1,9 +1,17 @@
-import {pgTable, serial, text, doublePrecision, pgEnum} from 'drizzle-orm/pg-core';
+import {pgTable, serial, text, doublePrecision, pgEnum, integer} from 'drizzle-orm/pg-core';
 
 export const events = pgTable('events', {
     id: serial('id').primaryKey(),
     name: text('name'),
     description: text('description'),
     price: doublePrecision('price'),
-    location: text('location')
+    location: text('location'),
+    managerId: integer('manager_id').references(() => user.id),
+});
+
+export const user = pgTable('users', {
+    id: serial('id').primaryKey(),
+    name: text('name'),
+    email: text('email'),
+    pubkey: text('pubkey')
 });
