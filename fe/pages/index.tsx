b967a6f5bfe4be7a375de1fa97185798bed90b4f
Tikhon <tikhon@wevestr.com>
2024-04-24 19:55:25 +0200

    better main page

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Inter } from "next/font/google";
-import {Typography} from "@mui/material";
+import {Stack, Typography} from "@mui/material";
 import {useAtomValue} from "jotai";
 import {userAtom} from "@/store/user";
 import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,13 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} w-full`}
     >
-      <Typography variant="h2">Welcome to Events.<br/> Please login with your wallet to proceed.</Typography>
+      <Stack sx={{display: 'flex', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{mt: 4}} variant="h3">Welcome to Events. Please login with your wallet to proceed.</Typography>
+          <Typography variant="h3">Please login with your wallet to proceed.</Typography>
+          <Image src="/party.gif" alt="party gif" width={200} height={200}/>
+      </Stack>
     </main>
   );
 }
